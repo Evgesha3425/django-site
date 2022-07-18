70a00fdb5046648d01563611768376a7c39960a4
Yauheni Eidelman <yauheni.skills@gmail.com>
2022-07-18 21:06:32 +0300

    Add new model News and setup SQLite studio on PC

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -118,6 +118,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Создаст папку media с загруженными картинками
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Указывает маршрут на папку с картинками
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
